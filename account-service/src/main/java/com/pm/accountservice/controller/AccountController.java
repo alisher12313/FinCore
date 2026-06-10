57f8a16d554c42d1ac5fd0f4a626848a20e85f84
Alisher <alibobaladno@gmail.com>
2026-06-11 03:28:16 +0500

    Controller for getting user profile and see preview of changed balance based on what currency he wants to see

--- account-service/src/main/java/com/pm/accountservice/controller/AccountController.java
+++ account-service/src/main/java/com/pm/accountservice/controller/AccountController.java
@@ -2,6 +2,7 @@ package com.pm.accountservice.controller;
 
 import com.pm.accountservice.dto.AccountResponseDto;
 import com.pm.accountservice.dto.CreateAccountRequestDto;
+import com.pm.accountservice.dto.ViewOrChangeCurrencyDto;
 import com.pm.accountservice.entity.Account;
 import com.pm.accountservice.service.AccountService;
 import jakarta.validation.Valid;
@@ -9,11 +10,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.Map;
 
 @Slf4j
@@ -34,6 +33,31 @@ public class AccountController {
         return ResponseEntity.status(HttpStatus.CREATED).body(Map.of(
                 "message", "Account created successfully",
                 "result", accountResponseDto
+        ));
+    }
+
+    @GetMapping("/my")
+    public ResponseEntity<?> getMyProfile() {
+        log.info("Getting my profile");
+
+        Account account = accountService.getMyProfile();
+        AccountResponseDto dto = accountService.toDto(account);
+
+        return ResponseEntity.ok(Map.of(
+                "message", "Account fetched successfully",
+                "result", dto
+        ));
+    }
+
+    @GetMapping("/my/balance/convert")
+    public ResponseEntity<?> getConvertedBalance(@Valid @RequestBody ViewOrChangeCurrencyDto dto) {
+        log.info("Getting converted balance");
+
+        BigDecimal converted = accountService.getConvertedBalance(dto);
+
+        return ResponseEntity.ok(Map.of(
+                "message", "Balance converted successfully",
+                "result", converted
         ));
     }
 }
