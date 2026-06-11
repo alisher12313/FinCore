f97afc4eafe7f68f99c7385a992ae5df20db549f
Alisher <alibobaladno@gmail.com>
2026-06-11 21:31:32 +0500

    imports

--- account-service/src/test/java/com/pm/accountservice/service/AccountServiceTest.java
+++ account-service/src/test/java/com/pm/accountservice/service/AccountServiceTest.java
@@ -21,8 +21,7 @@ import java.util.UUID;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class AccountServiceTest{
