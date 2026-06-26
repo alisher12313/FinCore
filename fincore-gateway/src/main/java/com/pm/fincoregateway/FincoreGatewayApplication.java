5551eb2eaa4d4fdb9b94b3af1ae8fcf924b32575
Alisher <alibobaladno@gmail.com>
2026-06-27 02:49:59 +0500

    project almost finished

---
+++ fincore-gateway/src/main/java/com/pm/fincoregateway/FincoreGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.pm.fincoregateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FincoreGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FincoreGatewayApplication.class, args);
+    }
+
+}
